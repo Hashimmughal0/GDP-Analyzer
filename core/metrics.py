4587866133562c2510550cb2af8a701d917d1b40
Hashimmughal0 <hashimmughal32@gmail.com>
2026-03-22 00:20:29 +0500

    multiprocessing metrics

---
+++ core/metrics.py
@@ -0,0 +1,20 @@
+﻿from multiprocessing import Lock, Value
+
+
+class QueueCounter:
+    def __init__(self) -> None:
+        self._value = Value('i', 0)
+        self._lock = Lock()
+
+    def increment(self) -> None:
+        with self._lock:
+            self._value.value += 1
+
+    def decrement(self) -> None:
+        with self._lock:
+            if self._value.value > 0:
+                self._value.value -= 1
+
+    def snapshot(self) -> int:
+        with self._lock:
+            return self._value.value
